5a86229ff760694132034bb16bbd5a091210831e
Kealsalas <brain_qiu@homtail.com>
2018-05-05 21:56:32 +0800

    for release

--- popup/action.js
+++ popup/action.js
@@ -5,7 +5,6 @@ function onError(error) {
 
 function setCurrentChoice(result) {
 	if(result == undefined || result == null || JSON.stringify(result) === "{}") {
-		//document.querySelector("#panel").innerHTML = "<div style='border: 1px solid black;margin: 1px; background-color:white;'>No patterns.</div>";
 		document.querySelector("#noList").classList.toggle("disabled");
 	} else {
 		var records = result.urls;
@@ -20,7 +19,7 @@ function setCurrentChoice(result) {
 			if(color == "") {
 				color = "white";
 			}
-			document.querySelector("#p" + element.no).innerHTML = "<strong>" + pattern + "</strong>";
+			document.querySelector("#p" + element.no).textContent = pattern;
 			document.querySelector("#p" + element.no).style = "margin: 1px; background:linear-gradient(to left, " + color + ", #f8f6f2);"
 		});
 	}
