127476899152507aed0bb40fd3899d47e45e4629
Kealsalas <brain_qiu@homtail.com>
2018-05-07 23:36:19 +0800

    v1.5.1

--- popup/action.js
+++ popup/action.js
@@ -4,39 +4,43 @@ function onError(error) {
 }
 
 function setCurrentChoice(result) {
-	if(result == undefined || result == null || JSON.stringify(result) === "{}") {
+	if (result == undefined || result == null || JSON.stringify(result) === "{}") {
 		document.querySelector("#noList").classList.toggle("disabled");
 	} else {
 		var records = result.urls;
-		records.forEach(function(element) {
+		records.forEach(function (element) {
 			var pattern = element.pattern;
 			var color = element.color;
-			if(pattern == "------") {
+			if (pattern == "------") {
 				pattern = "--Default--";
-			} else if(pattern == "") {
+			} else if (pattern == "") {
 				pattern = "--None--";
 			}
-			if(color == "") {
-				color = "white";
+			if (color == "") {
+				if (pattern === "--None--") {
+					color = "white";
+				} else {
+					color = records[0].color;
+				}
 			}
 			document.querySelector("#p" + element.no).textContent = pattern;
 			document.querySelector("#p" + element.no).style = "margin: 1px; background:linear-gradient(to left, " + color + ", #f8f6f2);"
 		});
 	}
 }
-var getting =  chrome.storage.sync.get(['urls'], function(result) {
-          setCurrentChoice(result);
-        });
+var getting = chrome.storage.sync.get(['urls'], function (result) {
+	setCurrentChoice(result);
+});
 document.querySelector("#options").addEventListener("click", JumpOptions);
 
 function JumpOptions(e) {
 	e.preventDefault();
 	function onOpened() {
-	  console.log(`Options page opened`);
+		console.log(`Options page opened`);
 	}
 
 	function onError(error) {
-	  console.log(`Error: ${error}`);
+		console.log(`Error: ${error}`);
 	}
 
 	chrome.runtime.openOptionsPage(onOpened);
